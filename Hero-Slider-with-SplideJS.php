fab3bba7f954da10d5f59d9225e317b6812ca2f6
nxtwebgen1 <nxtwebgen1@gmail.com>
2025-04-27 16:15:17 +0500

    Create Hero-Slider-with-SplideJS.php

---
+++ Hero-Slider-with-SplideJS.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Plugin Name:       Hero Slider with SplideJS
+ * Plugin URI:        https://github.com/NxtWebGen1/Hero-Slider-with-SplideJS
+ * Description:       Adds a customizable hero slider using SplideJS. Includes a shortcode [hero_slider] to display a responsive slider with image, heading, description, and button. Lightweight and easy to use.
+ * Version:           1.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Murslin Shehzad
+ * Author URI:        https://github.com/NxtWebGen1
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ */
